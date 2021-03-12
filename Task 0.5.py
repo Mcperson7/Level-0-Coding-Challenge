4768f0dee90a6ab3f534e21d64119756aac28372
PHILANI JASS <40671726+Mcperson7@users.noreply.github.com>
2021-03-12 13:12:16 +0200

    Second Commit

--- Task 0.5.py
+++ Task 0.5.py
@@ -1,15 +1,15 @@
 from math import sqrt as s
 
 #The code below calculates Area of a triangle using Heron's Formula!!! 3 sided triangle
-def area_of_a_triangle(x,y,z):
+def area_of_a_triangle(side1,side2,side3):
     
-    i = (x + y + z)/2 #This Calculates Half Of Tringles Perimeter
-    j = round(s((i*(i-x)*(i-y)*(i-z))),2) #We then find the area using this formula
+    i = (side1 + side2 + side3)/2 #This Calculates Half Of Tringles Perimeter
+    j = round(s((i*(i-side1)*(i-side2)*(i-side3))),2) #We then find the area using this formula
    
     print("The Area of this Triangle is: ", j)
 
-x = int(input("Size of size x: "))
-y = int(input("Size of side y: "))
-z = int(input("Size of side z: "))    
+side1 = int(input("Size of size side1: "))
+side2 = int(input("Size of side side2: "))
+side3 = int(input("Size of side side3: "))    
     
-area_of_a_triangle(x,y,z)
+area_of_a_triangle(side1,side2,side3)
