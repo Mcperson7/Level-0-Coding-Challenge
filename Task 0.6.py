0a61d97269df95abac3c7e91548901684b610eb6
PHILANI JASS <40671726+Mcperson7@users.noreply.github.com>
2021-03-13 21:41:55 +0200

    update2

--- Task 0.6.py
+++ Task 0.6.py
@@ -1,7 +1,7 @@
 def max_number(num1,num2,num3):
     
     if num1 > num2 and num1 > num3:
-        print (num)
+        print (num1)
     elif num2 > num3 and num2 > num1:
         print (num2)
     else:
@@ -16,7 +16,7 @@ max_number(num1,num2,num3)
 '''Below is the same updated fuction as top on
 the only difference is that this one accepts user input'''
 
-def max_number(num1,num2,num3):
+def maximum_num(num1,num2,num3):
     
     if num2 > num1 and num2 > num3:
         return num2
